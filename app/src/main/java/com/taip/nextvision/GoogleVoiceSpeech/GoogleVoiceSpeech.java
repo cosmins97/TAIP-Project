d734e76d519b8813b8be77a2106735889e5de156
Vlad Cirstean <vcirstean@bitdefender.com>
2020-11-03 21:21:36 +0200

    Add AOP

--- app/src/main/java/com/taip/nextvision/GoogleVoiceSpeech/GoogleVoiceSpeech.java
+++ app/src/main/java/com/taip/nextvision/GoogleVoiceSpeech/GoogleVoiceSpeech.java
@@ -2,13 +2,17 @@ package com.taip.nextvision.GoogleVoiceSpeech;
 
 import com.taip.nextvision.SpeechEngine;
 
+import hugo.weaving.DebugLog;
+
 public class GoogleVoiceSpeech implements SpeechEngine {
     @Override
+    @DebugLog
     public String speechToText() {
         return null;
     }
 
     @Override
+    @DebugLog
     public void textToSpeech(String text) {
 
     }
