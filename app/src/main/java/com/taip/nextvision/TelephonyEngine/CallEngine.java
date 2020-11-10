073051b831d568af60feabad0003ba6c9ad6f47b
Vlad Cirstean <vcirstean@bitdefender.com>
2020-11-10 20:07:45 +0200

    Add telephony methods

--- app/src/main/java/com/taip/nextvision/TelephonyEngine/CallEngine.java
+++ app/src/main/java/com/taip/nextvision/TelephonyEngine/CallEngine.java
@@ -1,8 +1,23 @@
 package com.taip.nextvision.TelephonyEngine;
 
+import android.content.ContentProviderOperation;
+import android.content.ContentProviderResult;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+
 import com.taip.nextvision.CommandEngine;
 
+import java.util.ArrayList;
+
 public class CallEngine implements CommandEngine {
+    Context context;
+
+    public CallEngine(Context context) {
+        this.context = context;
+    }
+
     @Override
     public String execute(String cmd) {
         Telephony telephony = Telephony.getInstance();
@@ -13,11 +28,54 @@ public class CallEngine implements CommandEngine {
     }
 
     private String callContact(Telephony telephony, String name) {
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String[] projection = new String[]{ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+        Cursor cursor = this.context.getContentResolver().query(uri, projection, null, null, null);
+
+        int idxName = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+        int idxNumber = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+
+        if (cursor.moveToFirst()) {
+            do {
+                if (cursor.getString(idxName).equals("YOUR CONTACT NAME")) {
+                    cursor.getString(idxNumber);
+                }
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
         return "";
     }
 
     private String createNewContact(String name, String number) {
-        return "";
+        ArrayList<ContentProviderOperation> contact = new ArrayList<ContentProviderOperation>();
+        contact.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
+                .build());
+
+        contact.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, 0)
+                .withValue(ContactsContract.RawContacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME, name)
+                .build());
+
+        contact.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, 0)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, number)
+                .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)
+                .build());
+
+
+        try {
+            ContentProviderResult[] results = this.context.getContentResolver().applyBatch(ContactsContract.AUTHORITY, contact);
+            return "Numarul a fost salvat";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Erorare";
+        }
     }
 
     private String callUnknownNumber(String number) {
