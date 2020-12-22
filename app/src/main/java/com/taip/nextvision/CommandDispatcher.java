9b6db302c90216e0efb42d771482cae45a12f0d5
Gitea <gitea@fake.local>
2020-12-22 21:04:50 +0200

    solved errors

--- app/src/main/java/com/taip/nextvision/CommandDispatcher.java
+++ app/src/main/java/com/taip/nextvision/CommandDispatcher.java
@@ -3,62 +3,39 @@ package com.taip.nextvision;
 import android.content.Context;
 import android.widget.Toast;
 
-import com.taip.nextvision.AlarmEngine.AlarmEngine;
-import com.taip.nextvision.BatteryEngine.BatteryEngine;
-import com.taip.nextvision.NotesEngine.NotesEngine;
 import com.taip.nextvision.TelephonyEngine.CallEngine;
 import com.taip.nextvision.TelephonyEngine.SMSEngine;
 import com.taip.nextvision.TimeEngine.DateEngine;
-import com.taip.nextvision.TimeEngine.TimeEngine;
 import com.taip.nextvision.directions.DirectionsEngine;
 
 public class CommandDispatcher {
     public String dispatch(Context context, String cmd) {
         Toast.makeText(context, cmd, Toast.LENGTH_SHORT).show();
         SpeechEngine.textToSpeech(cmd);
-        CommandEngine commandEngine = new CommandEngine() {
-            @Override
-            public String execute(String cmd) {
-                return "Nu am inteles comanda";
-            }
-        };
-
+        CommandEngine commandEngine;
+        String result = "";
         if (cmd.startsWith("suna ") || cmd.startsWith("apeleaza ") || cmd.startsWith("creeaza ")  || cmd.startsWith("adauga ") || cmd.startsWith("salveaza ")) {
             commandEngine = new CallEngine(context);
+            result = commandEngine.execute(cmd);
         } else if (cmd.equals("sms")) {
             commandEngine = new SMSEngine(context);
+            result = commandEngine.execute(cmd);
         } else if (cmd.equals("directii")) {
             commandEngine = new DirectionsEngine(context);
+            result = commandEngine.execute(cmd);
         } else if (cmd.equals("de la")) {
             commandEngine = new DirectionsEngine(context);
+            result = commandEngine.execute(cmd);
         } else if (cmd.equals("pana la")) {
             commandEngine = new DirectionsEngine(context);
+            result = commandEngine.execute(cmd);
         } else if (cmd.equals("data")) {
             commandEngine = new DateEngine(context);
-        } else if (cmd.equals("timp") || cmd.equals(("ora"))) {
-            commandEngine = new TimeEngine(context);
-        }else if (cmd.equals("baterie")) {
-            commandEngine = new BatteryEngine(context);
-        } else if (cmd.equals("incarca")) {
-            commandEngine = new BatteryEngine(context);
-        }else if (cmd.equals("mod economisire")) {
-            commandEngine = new BatteryEngine(context);
-        }else if (cmd.contains("seteaza alarma")) {
-            commandEngine = new AlarmEngine(context);
-        }else if (cmd.contains("sterge alarma")) {
-            commandEngine = new AlarmEngine(context);
-        }else if (cmd.contains("exista alarma")) {
-            commandEngine = new AlarmEngine(context);
-        }else if (cmd.contains("nota noua")) {
-            commandEngine = new NotesEngine(context);
-        }else if (cmd.contains("citeste note")) {
-            commandEngine = new NotesEngine(context);
-        }else if (cmd.contains("citeste ultima nota")) {
-            commandEngine = new NotesEngine(context);
-        }else if (cmd.contains("sterge ultima nota")) {
-            commandEngine = new NotesEngine(context);
+            result = commandEngine.execute(cmd);
+        } else {
+            result = "Nu am inteles comanda";
         }
-        String result = commandEngine.execute(cmd);
+
         Toast.makeText(context, result, Toast.LENGTH_SHORT).show();
         SpeechEngine.textToSpeech(result);
         return "";
